f1172974ffe96d02d6ca609ea6849035c0d1b707
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-07-03 23:48:05 -0400

    Fixing broken (easy)

--- buildSrc/src/main/java/Parser.java
+++ buildSrc/src/main/java/Parser.java
@@ -169,7 +169,7 @@ class Parser {
             case 8217: // back-tick `
                 return '\'';
             case 8221: // fancy open-quotes “
-            case 8220: // fancy close-quotes ”
+            case 8220: // fancy close-quotes [can't include the character because Windows complains]
                 return '\"';
             default:
                 return (char) intChar;
